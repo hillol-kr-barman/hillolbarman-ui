57b3acfc6bac3cfb1970ab28965b5194ce0f8942
hillol-kr-barman <hillolbarman@yahoo.com>
2026-06-02 12:44:01 +1000

    chore: remove ProjectCard component from ui library
    Component has been moved to the consuming app's native components directory.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,3 @@
 export { default as AlertDialogBox } from './AlertDialogBox'
 export { default as ConfirmationMessage } from './ConfirmationMessage'
 export { default as ShareDocumentModal } from './ShareDocumentModal'
-export { default as ProjectCard } from './ProjectCard'
